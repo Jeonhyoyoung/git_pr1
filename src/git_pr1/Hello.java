27662549fbb3212f85526dff753fa88c85063f9c
Jeon, Hyoyoung <hyoyoung412@gmail.com>
2021-10-25 21:55:17 +0900

    201814053-4

--- src/git_pr1/Hello.java
+++ src/git_pr1/Hello.java
@@ -1,11 +1,12 @@
-package git_exam1;
+package git_pr1;
 
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("201014099");
-		System.out.println("홍길동");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 201014099");
+		System.out.println("이름: 홍길동");
 	}
 
 }
+
